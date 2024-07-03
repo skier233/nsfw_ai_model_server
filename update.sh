b9ccc6fb9849731163eb69dd32a3b05072758163
skier233 <benchy222@gmail.com>
2024-07-03 17:15:16 -0400

    add linux update and start scripts

--- update.sh
+++ update.sh
@@ -1,3 +1,61 @@
 #!/bin/bash
+
+if ! command -v unzip &> /dev/null; then
+    echo "unzip could not be found, run sudo apt-get install unzip to install it."
+    exit 1
+fi
+
+# Function to get the latest release version from GitHub
+get_latest_release_version() {
+    apiUrl="https://api.github.com/repos/skier233/nsfw_ai_model_server/releases/latest"
+    latestRelease=$(curl -s $apiUrl | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo $latestRelease
+}
+
+# Function to read the local version from config.yaml
+get_local_version() {
+    configPath="./config/config.yaml"
+    if [[ -f "$configPath" ]]; then
+        localVersion=$(grep 'VERSION:' $configPath | awk '{print $2}')
+        echo $localVersion
+    else
+        echo "null"
+    fi
+}
+
+latestVersion=$(get_latest_release_version)
+localVersion=$(get_local_version)
+
+if [[ "$latestVersion" == "$localVersion" ]]; then
+    echo "You are up to date. Current version: $localVersion"
+    exit 0
+fi
+
+echo "Updating to latest version: $latestVersion from $localVersion"
+
+download="https://github.com/skier233/nsfw_ai_model_server/releases/latest/download/nsfw_ai_model_server.zip"
+zip="nsfw_ai_model_server.zip"
+tempDir="temp_nsfw_ai_model_server"
+
+echo "Downloading latest release"
+wget -O $zip $download
+
+echo "Extracting release files to temporary directory"
+mkdir -p $tempDir
+unzip -o $zip -d $tempDir
+
+echo "Copying files to the current directory"
+echo "debug 1"
+#TODO update update.sh at the end
+rsync -av --exclude='update.sh' $tempDir/* . || { echo "Failed to copy files."; exit 1; }
+echo "debug 2"
+echo "Cleaning up"
+rm -v -f $zip || { echo "Failed to remove the zip file."; exit 1; }
+rm -v -rf $tempDir || { echo "Failed to remove the temporary directory."; exit 1; }
+
+echo "Update complete"
+
+# Activate conda environment and update
+conda activate ai_model_server
 pip uninstall ai-processing -y
 conda env update --file ./environment-linux.yml
