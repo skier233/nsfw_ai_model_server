c6e35c7aeb7e25fb20718a6406350583f34dafa2
Jonathan Müller <52619285+LordofAgents@users.noreply.github.com>
2025-08-28 20:24:14 +0200

    Add AMD support to install script

--- install-linux.sh
+++ install-linux.sh
@@ -3,7 +3,18 @@
 # Activate conda environment and start server
 conda deactivate
 conda remove -n ai_model_server --all
-conda env create -f environment-linux.yml
-conda activate ai_model_server
+if [ "$1" = '--amd' ]; then
+    echo "AMD-Mode activated!"
+    # Create Env with special libs
+    conda env create -f environment-linux-amd.yml
+    conda activate ai_model_server
+    # Follow https://rocm.docs.amd.com/projects/radeon/en/latest/docs/install/wsl/install-pytorch.html
+    location=$(pip show torch | grep Location | awk -F ": " '{print $2}')
+    rm ${location}/torch/lib/libhsa-runtime64.so*
+    conda install -c conda-forge gcc=12.1.0
+else
+    conda env create -f environment-linux.yml
+    conda activate ai_model_server
+fi
 python migrate.py
 python server.py
